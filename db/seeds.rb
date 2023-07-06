ff7fd7888ceaf7fb7b99ed657228b18ea23d9ea6
luke <luke.nadackapadam@gmail.com>
2023-07-06 12:18:41 -0500

    create products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+product = Product.create(name: "Portable Charger", price: 50, image_url: "https://m.media-amazon.com/images/I/61Fs2NAB7CL.__AC_SX300_SY300_QL70_FMwebp_.jpg", description: "Fast-charging and long-lasting portable charger.")
+
+product = Product.create(name: "Portable Speaker", price: 90, image_url: "https://m.media-amazon.com/images/I/41DrkX8taNL._AC_SX466_.jpg", description: "Enjoy music andywhere, with amazing quality.")
+
+product = Product.create(name: "Audio-Technica Headphones", price: 170, image_url: "https://www.soundguys.com/wp-content/uploads/2022/03/audio-technica-ath-m50x-center-scaled.jpg", description: "Amazing sounding headphones.")
