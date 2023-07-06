5f1ebe26b18f8a56d52e159434ff32fdb9c5f27a
luke <luke.nadackapadam@gmail.com>
2023-07-06 12:58:16 -0500

    add routes for each product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,19 @@ class ProductsController < ApplicationController
     @products = Product.all
     render template: "products/index"
   end
+
+  def first_product
+    @product = Product.first
+    render template: "products/show"
+  end
+
+  def second_product
+    @product = Product.second
+    render template: "products/show"
+  end
+
+  def third_product
+    @product = Product.third
+    render template: "products/show"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get "/all_products", controller: "products", action: "all_products"
+
+  get "/first_product", controller: "products", action: "first_product"
+
+  get "/second_product", controller: "products", action: "second_product"
+
+  get "/third_product", controller: "products", action: "third_product"
 end
