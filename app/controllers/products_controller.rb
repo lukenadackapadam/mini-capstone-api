0274af2b04d3279a05d8ea8c1c27e623adfbc5bc
luke <luke.nadackapadam@gmail.com>
2023-07-07 14:06:18 -0500

    add new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,14 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params["id"])
     render :show
   end
+
+  def create
+    @product = Product.create(
+      name: "Playstation 5",
+      price: 500,
+      image_url: "https://assets-prd.ignimgs.com/2020/11/16/ps5-disc-1605555492213.png",
+      description: "Play video games!",
+    )
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "/products" => "products#all_products"
 
   get "/product/:id" => "products#one_product"
+
+  post "/products" => "products#create"
 end
