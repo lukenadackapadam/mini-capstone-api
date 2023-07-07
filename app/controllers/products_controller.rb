e5b296e69cfffd437baa24fbadfb4443ed47b9d2
luke <luke.nadackapadam@gmail.com>
2023-07-07 13:55:09 -0500

    add shortcuts

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,11 @@
 class ProductsController < ApplicationController
   def all_products
     @products = Product.all
-    render template: "products/index"
+    render :index
   end
-
-  # def first_product
-  #   @product = Product.first
-  #   render template: "products/show"
-  # end
-
-  # def second_product
-  #   @product = Product.second
-  #   render template: "products/show"
-  # end
-
-  # def third_product
-  #   @product = Product.third
-  #   render template: "products/show"
-  # end
 
   def one_product
     @product = Product.find_by(id: params["id"])
-    render template: "products/show"
+    render :show
   end
 end
