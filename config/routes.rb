89ea0974c16452bc71fb78d157ff75d11c494941
luke <luke.nadackapadam@gmail.com>
2023-07-10 11:46:32 -0500

    add update request

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get "/products" => "products#all_products"
 
-  get "/product/:id" => "products#one_product"
+  get "/products/:id" => "products#one_product"
 
   post "/products" => "products#create"
+
+  patch "/products/:id" => "products#update"
 
   # delete "/product/:id" => "products#destory"
 end
