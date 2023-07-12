911d939ba785036e98bf60b865309d6e0a678ba4
luke <luke.nadackapadam@gmail.com>
2023-07-12 13:46:41 -0500

    add migrations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,13 @@
 class Product < ApplicationRecord
   def is_discounted?
-    if price <= 10
-      return true
-    else
-      return false
-    end
+    price <= 10
   end
 
   def tax
-    return (price * 0.09).round(2)
+    (price * 0.09).round(2)
   end
 
-  def total 
-    return (price + tax)
+  def total
+    price + tax
   end
 end
---
+++ db/migrate/20230712184218_change_column_data_type.rb
@@ -0,0 +1,5 @@
+class ChangeColumnDataType < ActiveRecord::Migration[7.0]
+  def change
+    change_column :products, :description, :text
+  end
+end
