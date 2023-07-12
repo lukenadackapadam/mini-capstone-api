87c3d213a26d3dceb1e2ba0545bda1a6183644cc
luke <luke.nadackapadam@gmail.com>
2023-07-12 17:50:52 -0500

    change after reset

--- db/migrate/20230712185220_change_price_data_type.rb
+++ db/migrate/20230712185220_change_price_data_type.rb
@@ -1,5 +1,5 @@
 class ChangePriceDataType < ActiveRecord::Migration[7.0]
   def change
-    change_column :products, :price, :decimal
+    change_column :products, :price, :decimal, precision: 9, scale: 2
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_12_184503) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_185220) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.decimal "price"
+    t.decimal "price", precision: 9, scale: 2
     t.string "image_url"
     t.text "description"
     t.datetime "created_at", null: false
