c364bdc697f3e3cc43bab78a11432ee22fef3542
luke <luke.nadackapadam@gmail.com>
2023-07-20 13:35:00 -0500

    Add relationships

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ApplicationRecord
 
   belongs_to :supplier
   has_many :orders
+  has_many :category_products
+  has_many :categories, through: :category_products
 
   # def supplier
   #   Supplier.find_by(id: supplier_id)
