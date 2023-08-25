697c30a1b40c3ad651446cc3ca4e58dbb42b2477
luke <luke.nadackapadam@gmail.com>
2023-08-25 13:38:15 -0500

    Edit seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@ Supplier.create!([
   { name: "Costco", email: "Costco@gmail.com", phone_number: "1112223456" },
 ])
 
-product = Product.create(name: "Portable Charger", price: 50, image_url: "https://m.media-amazon.com/images/I/61Fs2NAB7CL.__AC_SX300_SY300_QL70_FMwebp_.jpg", description: "Fast-charging and long-lasting portable charger.")
+product = Product.create(name: "Portable Charger", price: 50, image_id: 1, description: "Fast-charging and long-lasting portable charger.", supplier_id: 1)
 
-product = Product.create(name: "Portable Speaker", price: 90, image_url: "https://m.media-amazon.com/images/I/41DrkX8taNL._AC_SX466_.jpg", description: "Enjoy music andywhere, with amazing quality.")
+product = Product.create(name: "Portable Speaker", price: 90, image_id: 2, description: "Enjoy music andywhere, with amazing quality.", supplier_id: 1)
 
-product = Product.create(name: "Audio-Technica Headphones", price: 170, image_url: "https://www.soundguys.com/wp-content/uploads/2022/03/audio-technica-ath-m50x-center-scaled.jpg", description: "Amazing sounding headphones.")
+product = Product.create(name: "Audio-Technica Headphones", price: 170, image_id: 3, description: "Amazing sounding headphones.", supplier_id: 2)
